6abc57b19212ab763cdbe8d3629c1113a6a6b928
Paulo Henrique <p.henriquerm@hotmail.com>
2020-03-03 16:28:39 -0300

    #2 Criando Classes DTO

---
+++ Produtos.Aplicacao/DTO/ProdutoDTO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Produtos.Aplicacao.DTO
+{
+    public class ProdutoDTO : DTOBase
+    {
+        public string Descricao { get; set; }
+        public string Marca { get; set; }
+        public int NumeroLote { get; set; }
+        public double Preco { get; set; }
+    }
+}
