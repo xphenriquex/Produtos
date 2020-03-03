3a6e58886415bb890d7d1779a7e5856af3a4a628
Paulo Henrique <p.henriquerm@hotmail.com>
2020-03-03 14:36:44 -0300

    #1 Criando entidades base e produto

---
+++ Produtos.Dominio/Entidades/EntidadeBase.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Produtos.Dominio.Entidades
+{
+    public class EntidadeBase
+    {
+        public int Id { get; set; }
+
+    }
+}
---
+++ Produtos.Dominio/Entidades/Produto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Produtos.Dominio.Entidades
+{
+    public class Produto : EntidadeBase
+    {
+        public string Descricao { get; set; }
+        public string Marca { get; set; }
+        public int NumeroLote { get; set; }
+        public double Preco { get; set; }
+
+    }
+}
