1535a22b5faeb1a3243b49152390d07262c32da8
Paulo Henrique <p.henriquerm@hotmail.com>
2020-03-03 14:46:09 -0300

    #1 Criando classes de serviços da camada de dominio

---
+++ Produtos.Dominio/Servicos/ProdutoServico.cs
@@ -0,0 +1,12 @@
+﻿using Produtos.Dominio.Entidades;
+using Produtos.Dominio.Interfaces.Repositorios;
+
+namespace Produtos.Dominio.Servicos
+{
+    public class ProdutoServico : ServicoBase<Produto>
+    {
+        public ProdutoServico(IProdutoRepositorio repositorio) : base(repositorio)
+        {
+        }
+    }
+}
---
+++ Produtos.Dominio/Servicos/ServicoBase.cs
@@ -0,0 +1,41 @@
+﻿using Produtos.Dominio.Entidades;
+using Produtos.Dominio.Interfaces.Repositorios;
+using Produtos.Dominio.Interfaces.Servicos;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Produtos.Dominio.Servicos
+{
+    public class ServicoBase<TEntidade> : IServicoBase<TEntidade> where TEntidade : EntidadeBase
+    {
+        protected readonly IRepositorioBase<TEntidade> _repo;
+
+        public ServicoBase(IRepositorioBase<TEntidade> repositorio)
+        {
+            _repo = repositorio;
+        }
+
+        public void Atualizar(TEntidade entity)
+        {
+            _repo.Atualizar(entity);
+        }
+
+        public void Cadastrar(TEntidade entity)
+        {
+            _repo.Cadastrar(entity);
+        }
+
+        public void Deletar(TEntidade entity)
+        {
+            _repo.Deletar(entity);
+
+        }
+
+        public Task<TEntidade> SalvarAsync(TEntidade entity)
+        {
+            return _repo.SalvarAsync(entity);
+        }
+    }
+}
