901d39d1fbd5ffcb93b4d0c556da91af2c30e654
Paulo Henrique <p.henriquerm@hotmail.com>
2020-03-03 22:24:16 -0300

    #1 Adicionando interface IProdutoServico no ProdutoServico

--- Produtos.Dominio/Servicos/ProdutoServico.cs
+++ Produtos.Dominio/Servicos/ProdutoServico.cs
@@ -1,9 +1,10 @@
 ﻿using Produtos.Dominio.Entidades;
 using Produtos.Dominio.Interfaces.Repositorios;
+using Produtos.Dominio.Interfaces.Servicos;
 
 namespace Produtos.Dominio.Servicos
 {
-    public class ProdutoServico : ServicoBase<Produto>
+    public class ProdutoServico : ServicoBase<Produto>, IProdutoServico
     {
         public ProdutoServico(IProdutoRepositorio repositorio) : base(repositorio)
         {
