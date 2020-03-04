b8a456786ed3c825a8af7cd012be2d606de25085
Paulo Henrique <p.henriquerm@hotmail.com>
2020-03-03 22:22:34 -0300

    configurando injetor de dependencia

---
+++ Produtos.Infra.IoC/InjetorDepencias.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Produtos.Aplicacao.Interfaces;
+using Produtos.Aplicacao.Servicos;
+using Produtos.Dominio.Interfaces.Repositorios;
+using Produtos.Dominio.Interfaces.Servicos;
+using Produtos.Dominio.Servicos;
+using Produtos.Infra.Data.Repositorios;
+
+namespace Produtos.Infra.IoC
+{
+    public class InjetorDepencias
+    {
+        public static void Registrar(IServiceCollection svcCollection)
+        {
+            //Aplicação
+            svcCollection.AddScoped(typeof(IAppbase<,>), typeof(ServicoAppBase<,>));
+            svcCollection.AddScoped<IProdutoApp, ProdutoApp>();
+
+            //Domínio
+            svcCollection.AddScoped(typeof(IServicoBase<>), typeof(ServicoBase<>));
+            svcCollection.AddScoped<IProdutoServico, ProdutoServico>();
+
+            //Repositorio
+            svcCollection.AddScoped(typeof(IRepositorioBase<>), typeof(RepositorioBase<>));
+            svcCollection.AddScoped<IProdutoRepositorio, ProdutoRepositorio>();
+
+        }
+    }
+}
